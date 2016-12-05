0a73ead6f5461c66a9800637c21d03a59a46bb5d
John Gu <johng@tune.com>
2016-12-05 15:26:59 -0800

    Rebuild modules for Titanium 6.0.0.GA

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Please see the Quick Start guide here:
 
-[Titanium Quick Start](https://developers.mobileapptracking.com/titanium-plugin/)
+[Titanium Quick Start](https://developers.tune.com/sdk/titanium-quick-start/)
--- mobile/android/build.sh
+++ mobile/android/build.sh
@@ -3,6 +3,7 @@ rm -rf build
 
 cp build.properties.dist build.properties
 perl -pi -e "s|\<replace_with_titanium_home_path\>|$TITANIUM_LOCATION|" build.properties
+perl -pi -e "s|\<replace_with_titanium_version>|6.0.0.GA|" build.properties
 perl -pi -e "s|\<replace_with_android_sdk_path\>|$ANDROIDSDK_LOCATION|" build.properties
 perl -pi -e "s|\<replace_with_android_ndk_path\>|$ANDROIDNDK_LOCATION|" build.properties
 
